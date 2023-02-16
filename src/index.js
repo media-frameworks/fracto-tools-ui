125fae434f3ea5ec5e1e62e4adf5367d454f7899
EC2 Default User <mikehallvideo@gmail.com>
2023-02-16 01:43:33 +0000

    google auth

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import {GoogleOAuthProvider} from '@react-oauth/google';
+import AppLoginPage from "common/app/AppLoginPage"
+import google_clident_data from "admin/google_client_data.json"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+   <GoogleOAuthProvider clientId={google_clident_data.web.client_id}>
+      <React.StrictMode>
+         <AppLoginPage app_name={"fracto-tools"}/>
+      </React.StrictMode>
+   </GoogleOAuthProvider>,
+   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/PageMain.jsx
@@ -0,0 +1,14 @@
+import {Component} from 'react';
+import PropTypes from 'prop-types';
+import styled from "styled-components";
+
+export class PageMain extends Component {
+
+   static propTypes = {}
+
+   render() {
+      return "PageMain fracto-tools"
+   }
+}
+
+export default PageMain;
